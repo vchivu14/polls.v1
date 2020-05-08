19a543ccb1a320ab2ff152986d8a6781544385f6
vchivu <chivu.vlad@yahoo.com>
2020-05-08 16:54:22 +0300

    Changed to local views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,53 +8,53 @@ from django.utils import timezone
 # from django.template import loader
 
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     # template = loader.get_template("polls/index.html")
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     # return HttpResponse(template.render(context, request))
-#     return render(request, 'polls/index.html', context)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = "latest_question_list"
-    def get_queryset(self):
-        # return Question.objects.order_by('-pub_date')[:5]
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
-
-
-
-# def detail(request, question_id):
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("Question does not exist")
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question':question})
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = "latest_question_list"
+#     def get_queryset(self):
+#         # return Question.objects.order_by('-pub_date')[:5]
+#         return Question.objects.filter(
+#             pub_date__lte=timezone.now()
+#         ).order_by('-pub_date')[:5]
 
 
 
-# def results(request, question_id):
-#     # response = "You're looking at the results of question %s"
-#     # return HttpResponse(response % question_id)
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question':question})
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-    def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+#     def get_queryset(self):
+#         return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+
+def results(request, question_id):
+    # response = "You're looking at the results of question %s"
+    # return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
+#     def get_queryset(self):
+#         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 
